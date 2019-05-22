0bc7100fd68ae21cf8d51638c927123ed6efbbf0
daydreamt <nmoraitakis@yahoo.com>
2019-05-22 23:13:57 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='MRF',
+   version='0.001',
+   description='Binary MRF in Pytorch',
+   author='daydreamt',
+   packages=['MRF'], 
+   install_requires=['torch']
+)
